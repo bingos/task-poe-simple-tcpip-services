4c7e6a3241216848f3ad2e32534c634ec7027b6c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-03-30 12:23:00 +0100

    Switch dist to Dist::Zilla

--- lib/Task/POE/Simple/TCPIP/Services.pm
+++ lib/Task/POE/Simple/TCPIP/Services.pm
@@ -1,16 +1,13 @@
 package Task::POE::Simple::TCPIP::Services;
 
+#ABSTRACT: A Task to install all POE simple TCP/IP services modules.
+
 use strict;
 use warnings;
-use vars qw($VERSION);
 
-$VERSION = '1.08';
+'Simples';
 
-1;
-
-=head1 NAME
-
-Task::POE::Simple::TCPIP::Services - A Task to install all POE simple TCP/IP services modules.
+=pod
 
 =head1 SYNOPSIS
 
@@ -18,7 +15,7 @@ Task::POE::Simple::TCPIP::Services - A Task to install all POE simple TCP/IP ser
 
 =head1 DESCRIPTION
 
-Task::POE::Simple::TCPIP::Services will install all the L<POE> modules that provide what 
+Task::POE::Simple::TCPIP::Services will install all the L<POE> modules that provide what
 Microsoft Windows terms "Simple TCP/IP Services", namely:
 
   Quote of the Day Protocol
@@ -43,12 +40,4 @@ The following modules will be installed:
 
   POE::Component::Server::Time 1.10
 
-=head1 LICENSE
-
-This is free software, released under the same terms as perl itself.
-
-=head1 AUTHOR
-
-Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
-
-=cut 
+=cut
